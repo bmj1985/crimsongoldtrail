c4a0a0a0f7e54c8550c16a6aa7fb7003bc3fd985
Brandon Johnson <bmj1985@gmail.com>
2018-04-26 08:07:08 -0600

    form redirect to home

--- src/components/AddNewBusiness.jsx
+++ src/components/AddNewBusiness.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
 import '../styles/components/AddNewBusiness.css'
+import { Link } from 'react-router-dom'
 
 export default class AddNewBusiness extends React.Component {
   render () {
@@ -13,104 +14,76 @@ export default class AddNewBusiness extends React.Component {
           </FormGroup>
           <FormGroup>
             <Label for="alumName">Alumnus Name</Label>
-            <Input type="text" name="alumName" id="alumName" placeholder="Jane Doe" />
+            <Input type="text" name="alumName" id="alumName" placeholder="ex. Duane Michals, Sandra Dallas" />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="title">Title at Business</Label>
+            <Input type="text" name="titleAtBusiness" id="titleAtBusiness" placeholder="ex. Owner, CEO, etc." />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="">Degree</Label>
+            <Input type="text" name="" id="" placeholder="ex. B.A., B.S., M.S., etc." />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="">Class</Label>
+            <Input type="text" name="" id="" placeholder="ex. 1984" />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="">Field</Label>
+            <Input type="text" name="field" id="fieldForm" placeholder="ex. Chemistry, Business, etc." />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="">Business Description</Label>
+            <Input type="textarea" name="businessDescriptionForm" id="businessDescripstionForm" placeholder="ex. We are sustainable food advocates by nature and embrace a planet-friendly lifestyle. We share this passion for responsibly grown food through our pizzas, salads, soups, and more." />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="">Business Url</Label>
+            <Input type="url" name="businessUrl" id="businessUrlForm" placeholder="wwww.yourbusinessname.com" />
           </FormGroup>
         </FormGroup>
         <FormGroup id="col2">
           <FormGroup>
-            <Label for="businessName">Business Name</Label>
-            <Input type="text" name="businessName" id="" placeholder="Your business name, ex. Sazza Pizza + Salads" />
+            <Label for="locationName">Location Name</Label>
+            <Input type="text" name="locationName" id="locationNameForm" placeholder="ex. Stanley Marketplace" />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="address">Address</Label>
+            <Input type="text" name="address" id="addressForm" placeholder="ex. 1984 George Orwell Dr." />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="suite">Suite/Unit Number</Label>
+            <Input type="text" name="suite" id="suiteForm" placeholder="#4a" />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="city">City</Label>
+            <Input type="text" name="city" id="cityForm" placeholder="Denver" />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="zipCode">Zip Code</Label>
+            <Input type="text" name="zipCode" id="zipCode" placeholder="ex. 80202" />
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="">Profile Image Url</Label>
+            <Input type="url" name="imageUrl" id="imageUrl" placeholder="ex. http://www.domainname.com/image.jpg" />
+          </FormGroup>
+          <FormGroup id="coordinatesFormGroup">
+            <div id="latitudeGroup">
+              <Label for="latitude" class="coordinateLabel">Latitude</Label>
+              <Input type="number" name="latitude" id="latitudeInput" placeholder="" />
+            </div>
+            <div id="longitudeGroup">
+              <Label for="longitude" class="coordinateLabel">Longitude</Label>
+              <Input type="number" name="longitude" id="longitudeInput" placeholder="" />
+            </div>
           </FormGroup>
           <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="">Label</Label>
-            <Input type="text" name="" id="" placeholder="" />
+            <Label for="">Phone Number</Label>
+            <Input type="tel" name="phoneNumber" id="phoneNumberForm" placeholder="ex. 303-999-1984" />
           </FormGroup>
         </FormGroup>
-        {/* <div id="submitNewBusinessWrapper"> */}
-        <div id="submitNewBusiness">
+        <Link to="/home" id="submitNewBusiness">
           <Button id="submitNewBusinessButton">Submit</Button>
-        </div>
-        {/* </div> */}
+        </Link>
       </Form>
     )
   }
