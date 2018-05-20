b062349d56170c98380e869eda799901c6307a37
Brandon Johnson <bmj1985@gmail.com>
2018-05-20 11:13:56 -0600

    sun may 20

--- src/views/CGGoogleMap.jsx
+++ src/views/CGGoogleMap.jsx
@@ -7,49 +7,49 @@ import { Card, Button, Fade } from 'reactstrap'
 import '../styles/components/CGGoogleMap.css'
 
 export class CGGoogleMap extends Component {
-         constructor(props) {
-           super(props);
-           this.state = { closeAll: false, showHideCard: "hidden", business: {} };
-         }
+  constructor (props) {
+    super(props)
+    this.state = { closeAll: false, showHideCard: 'hidden', business: {} }
+  }
 
-         toggleSideCard(e) {
-           var css = this.state.showHideCard === "hidden" ? "businessModal" : "hidden";
-           this.setState({
-             showHideCard: css,
-             business: e.business.business
-           })
-         }
+  toggleSideCard (e) {
+    const css = this.state.showHideCard === 'hidden' ? 'businessModal' : 'hidden'
+    this.setState({
+      showHideCard: css,
+      business: e.business.business
+    })
+  }
 
-         render() {
-           if (this.props.nearMe === false) {
-             return <div className="mapContainer">
-                 <Map className="Google-Map" google={this.props.google} zoom={12} ControlPosition="BOTTOM_LEFT" initialCenter={{ lat: 39.757538, lng: -104.939617 }} containerStyle={{ height: "82vh" }}>
-                   {this.props.businesses.map(business => {
-                     return business.Location.map(location => {
-                       return <Marker id="marker" onClick={this.toggleSideCard.bind(this)} position={{ lat: location.Coordinates[0], lng: location.Coordinates[1] }} icon={images.Imgur_Icon} business={{ business }} />;
-                     });
-                   })}
-                 </Map>
-                 <Card id={this.state.showHideCard} size="sm">
-                   <Sidebar id="sidebar" business={this.state.business} />
-                 </Card>
-               </div>;
-           } else {
-             return <div className="mapContainer">
-                 <Map className="Google-Map" google={this.props.google} zoom={14} ControlPosition="BOTTOM_LEFT" initialCenter={{ lat: 39.757538, lng: -104.939617 }} centerAroundCurrentLocation containerStyle={{ height: "82vh" }}>
-                   {this.props.businesses.map(business => {
-                     return business.Location.map(location => {
-                       return <Marker id="marker" onClick={this.toggleSideCard.bind(this)} position={{ lat: location.Coordinates[0], lng: location.Coordinates[1] }} icon={{ url: images.Imgur_Icon }} business={{ business }} />;
-                     });
-                   })}
-                 </Map>
-                 <Card id={this.state.showHideCard} size="sm">
-                   <Sidebar id="sidebar" business={this.state.business} />
-                 </Card>
-               </div>;
-           }
-         }
-       }
+  render () {
+    if (this.props.nearMe === false) {
+      return <div className="mapContainer">
+        <Map className="Google-Map" google={this.props.google} zoom={12} ControlPosition="BOTTOM_LEFT" initialCenter={{ lat: 39.757538, lng: -104.939617 }} containerStyle={{ height: '82vh' }}>
+          {this.props.businesses.map(business => {
+            return business.Location.map(location => {
+              return <Marker id="marker" onClick={this.toggleSideCard.bind(this)} position={{ lat: location.Coordinates[0], lng: location.Coordinates[1] }} icon={images.Imgur_Icon} business={{ business }} />
+            })
+          })}
+        </Map>
+        <Card id={this.state.showHideCard} size="sm">
+          <Sidebar id="sidebar" business={this.state.business} />
+        </Card>
+      </div>
+    } else {
+      return <div className="mapContainer">
+        <Map className="Google-Map" google={this.props.google} zoom={14} ControlPosition="BOTTOM_LEFT" initialCenter={{ lat: 39.757538, lng: -104.939617 }} centerAroundCurrentLocation containerStyle={{ height: '82vh' }}>
+          {this.props.businesses.map(business => {
+            return business.Location.map(location => {
+              return <Marker id="marker" onClick={this.toggleSideCard.bind(this)} position={{ lat: location.Coordinates[0], lng: location.Coordinates[1] }} icon={{ url: images.Imgur_Icon }} business={{ business }} />
+            })
+          })}
+        </Map>
+        <Card id={this.state.showHideCard} size="sm">
+          <Sidebar id="sidebar" business={this.state.business} />
+        </Card>
+      </div>
+    }
+  }
+}
 
 const GoogleMapsApiKey = config.REACT_APP_GOOGLE_MAPS_API_KEY
 export default GoogleApiWrapper({
